f1426a3cced230f3374f9b66b2a338cd9f453161
akhil <aj17kr@gmail.com>
2018-02-28 23:08:39 +0530

    Added GET todos/:id route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser=require('body-parser');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {Users}=require('./models/users');
+var {ObjectID}=require("mongodb");
 
 var app=express();
 
@@ -34,6 +35,26 @@ app.get('/todos',(req,res)=>{
   });
 });
 
+// Get Todo when user inputs value.
+app.get('/todos/:id',(req,res)=>{
+  let id=req.params.id;
+
+  // check if enterted ID is a valid ID. //
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send("Invalid ID");
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send("ID not found");
+    }
+    res.send(todo);
+  },(err)=>{
+    res.status(400).send();
+  });
+});
+
+/* ===============Starting Express Server================ */
 app.listen(3000,()=>{
   console.log("Started on port 3000");
 });
