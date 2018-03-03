4807310c949d3b97579ec4731680e8a0bc8a4803
akhil <aj17kr@gmail.com>
2018-03-04 03:47:08 +0530

    added mlab mongodb url directly

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@ mongoose.Promise=global.Promise;
 
 var url="mongodb://localhost:27017/nodeMongoDbTodo";
 //Use dynamic URL in heroku or local MongoDB url.
-mongoose.connect(process.env.MONGODB_URI || url);
+mongoose.connect("mongodb://aj:000000@ds155278.mlab.com:55278/heroku-nodetodo-api");
 
 module.export={mongoose};
