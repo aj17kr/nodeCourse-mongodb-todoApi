4c82426af7467e4ac6af9bacdcacb80d19ce9966
akhil <aj17kr@gmail.com>
2018-03-04 04:11:41 +0530

    updated for mlab connection with heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,12 @@ const mongoose=require("mongoose");
 
 mongoose.Promise=global.Promise;
 
-var url="mongodb://localhost:27017/nodeMongoDbTodo";
+//var url="mongodb://localhost:27017/nodeMongoDbTodo";
 //Use dynamic URL in heroku or local MongoDB url.
-mongoose.connect("mongodb://aj:000000@ds155278.mlab.com:55278/heroku-nodetodo-api");
+let db = {
+  localhost: 'mongodb://localhost:27017/nodeMongoDbTodo',
+  mlab: 'mongodb://aj:000000@ds155278.mlab.com:55278/heroku-nodetodo-api'
+};
+mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
 
 module.export={mongoose};
