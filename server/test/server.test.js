9c392f5909e1ae47a65c73261277985c4aadc5ad
akhil <aj17kr@gmail.com>
2018-03-03 11:43:30 +0530

    Added /GET Todo test describe block

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,11 +4,16 @@ const request=require("supertest");
 const {app}=require('./../server');
 const {Todo}=require('./../models/todo');
 
+let todos=[{text:"first text todo"},{text:"second text todo"}];
+
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done());
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+  }).then(()=>done());
 });
 
 describe("POST /todos",()=>{
+
 //Should add new todo
   it('should create a new todo',(done)=>{
     var text="From a testing todo";
@@ -24,7 +29,7 @@ describe("POST /todos",()=>{
       return done(err);
       }
 
-    Todo.find().then((todos)=>{
+    Todo.find({text}).then((todos)=>{
       expect(todos.length).toBe(1);
       expect(todos[0].text).toBe(text);
       done();
@@ -43,12 +48,24 @@ it('should not create todo with invalid body data',(done)=>{
     return done(err);
     }
 
-    Todo.find().then((todos)=>{
-      expect(todos.length).toBe(0);
-      done();
+  Todo.find().then((todos)=>{
+    expect(todos.length).toBe(2); //toBe value enterted is 2 cause in beforeEach we've added 2 values in collection. So before every "it" block runs that whole beforeEach block runs first.
+    done();
     }).catch((e)=>done(e));
-
   });
 });
 
 });
+
+//Describe method for Get Todos
+describe("GET /Todos",()=>{
+  it("should return todo using GET METHOD",(done)=>{
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+    });
+  });
