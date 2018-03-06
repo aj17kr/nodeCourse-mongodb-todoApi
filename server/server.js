a5ce160fe6cf89034553a82b611347d595d69fb4
akhil <aj17kr@gmail.com>
2018-03-06 21:40:15 +0530

    Add /todo/:id Update route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var express=require('express');
-var bodyParser=require('body-parser');
+const express=require('express');
+const bodyParser=require('body-parser');
+const _ = require('lodash');
+const {ObjectID}=require("mongodb");
 
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {Users}=require('./models/users');
-var {ObjectID}=require("mongodb");
 
 var app=express();
 
@@ -74,6 +75,34 @@ app.delete('/todos/:id',(req,res)=>{
     });
   });
 
+  // ============ Update Todo ============= //
+
+app.patch('/todos/:id',(req,res)=>{
+  let id=req.params.id;
+  let body=_.pick(req.body,['text','completed'])
+
+  // check if enterted ID is a valid ID. //
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send("Invalid ID");
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt=new Date().getTime();
+  }
+    else{
+      body.completed=false;
+      body.completedAt=null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set:body} , {new:true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send("todo not found");
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+  });
 
 /* ===============Starting Express Server================ */
 //Dynamic port is used alongwith 3000 port, if used in heroku dynamic assigned port is used ,Locally 3000 port is used.
