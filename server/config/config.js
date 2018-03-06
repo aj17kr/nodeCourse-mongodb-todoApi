aef104ea1f155f2155891e8479dbf9dda8980286
akhil <aj17kr@gmail.com>
2018-03-07 01:46:38 +0530

    exp

--- server/config/config.js
+++ server/config/config.js
@@ -2,29 +2,29 @@ var env=process.env.NODE_ENV || "development";
 console.log(`***** ${env} ******`);
 
 
-// if(env==="development"){
-//   process.env.PORT=3000;
-//   process.env.MONGODB_URI="mongodb://localhost:27017/nodeMongoDbTodo";
-// }
-// else if(env="test"){
-//   process.env.PORT=3000;
-//   process.env.MONGODB_URI="mongodb://localhost:27017/nodeMongoDbTodoTest";
-// }
-// else if(env="production"){
-//
-//   process.env.MONGODB_URI="mongodb://aj:000000@ds155278.mlab.com:55278/heroku-nodetodo-api";
-// }
-
-if(env==="production"){
-  process.env.MONGODB_URI="mongodb://aj:000000@ds155278.mlab.com:55278/heroku-nodetodo-api";
+if(env==="development"){
+  process.env.PORT=3000;
+  process.env.MONGODB_URI="mongodb://localhost:27017/nodeMongoDbTodo";
 }
 else if(env==="test"){
   process.env.PORT=3000;
   process.env.MONGODB_URI="mongodb://localhost:27017/nodeMongoDbTodoTest";
 }
 else{
-  process.env.PORT=3000;
-  process.env.MONGODB_URI="mongodb://localhost:27017/nodeMongoDbTodo";
+
+  process.env.MONGODB_URI="mongodb://aj:000000@ds155278.mlab.com:55278/heroku-nodetodo-api";
 }
+
+// if(env==="production"){
+//   process.env.MONGODB_URI="mongodb://aj:000000@ds155278.mlab.com:55278/heroku-nodetodo-api";
+// }
+// else if(env==="test"){
+//   process.env.PORT=3000;
+//   process.env.MONGODB_URI="mongodb://localhost:27017/nodeMongoDbTodoTest";
+// }
+// else{
+//   process.env.PORT=3000;
+//   process.env.MONGODB_URI="mongodb://localhost:27017/nodeMongoDbTodo";
+// }
 console.log(`****** ${process.env.MONGODB_URI}*******`);
 console.log(`****** ${process.env.PORT} ********`);
