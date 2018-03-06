7b8cdad719b29f1fc8edb3a7aab4ecca6bb3700a
akhil <aj17kr@gmail.com>
2018-03-07 01:14:59 +0530

    add logs to see port and MongoDB URI

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,7 @@
 var env=process.env.NODE_ENV || "development";
 console.log(`***** ${env} ******`);
 
+
 if(env==="development"){
   process.env.PORT=3000;
   process.env.MONGODB_URI="mongodb://localhost:27017/nodeMongoDbTodo";
@@ -9,3 +10,9 @@ else if(env="test"){
   process.env.PORT=3000;
   process.env.MONGODB_URI="mongodb://localhost:27017/nodeMongoDbTodoTest";
 }
+console.log(process.env.MONGODB_URI);
+console.log(process.env.PORT);
+// else{
+//   process.env.PORT=process.env.PORT;
+//   process.env.MONGODB_URI="mongodb://localhost:27017/nodeMongoDbTodoTest";
+// }
