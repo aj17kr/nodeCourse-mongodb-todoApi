803cc383cb58bd8ab79add946ce67ee519bb26dd
akhil <aj17kr@gmail.com>
2018-03-07 01:34:54 +0530

    another exp

--- server/config/config.js
+++ server/config/config.js
@@ -10,10 +10,9 @@ else if(env="test"){
   process.env.PORT=3000;
   process.env.MONGODB_URI="mongodb://localhost:27017/nodeMongoDbTodoTest";
 }
-console.log(`****** ${process.env.MONGODB_URI}*******`);
-console.log(`****** ${process.env.PORT} ********`);
-
 else if(env="production"){
-  process.env.PORT=process.env.PORT;
+
   process.env.MONGODB_URI="mongodb://aj:000000@ds155278.mlab.com:55278/heroku-nodetodo-api";
 }
+console.log(`****** ${process.env.MONGODB_URI}*******`);
+console.log(`****** ${process.env.PORT} ********`);
